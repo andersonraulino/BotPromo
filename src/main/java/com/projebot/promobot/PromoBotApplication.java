e6ab0752b71c3a9a3d5dc89102b6184ae868aee1
andersonraulino <anderson.raul4631@gmail.com>
2026-05-06 18:31:52 -0300

    Criando base da aplicação

---
+++ src/main/java/com/projebot/promobot/PromoBotApplication.java
@@ -0,0 +1,36 @@
+/*package com.projebot.promo_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoBotApplication.class, args);
+	}
+
+}*/
+
+// -------- CÓDIGO PARA FORÇAR O SPRING IGNORAR O BANCO DE DADOS -----------
+/*import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PromoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromobotApplication.class, args);
+    }
+}*/
+package com.projebot.promobot;
+
+import org.springframework.boot.SpringApplication;
+//*import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class PromoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoBotApplication.class, args);
+    }
+}
